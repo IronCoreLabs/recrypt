43f926c6f292df44aa5d28a1f6044fe1c87669cd
Ernie Turner <ernie.turner@ironcorelabs.com>
2018-09-11 11:44:30 -0600

    Change JS syntax in benchmarks to plain ES5

--- core/js/src/test/encryptedvalue.js
+++ core/js/src/test/encryptedvalue.js
@@ -1,18 +1,24 @@
 /**
  * Generate a self contained level 1 encrypted value. Results in the encrypted value and the private key that can be used to decrypt that value.
  */
-function generateEncryptedLevel1(PreCrypt, signingKeys, done){
-    const publicSigningKey = {bytes: signingKeys.publicKey};
-    const privateSigningKey = {bytes: signingKeys.secretKey};
+function generateEncryptedLevel1(PreCrypt, signingKeys, done) {
+    var publicSigningKey = {bytes: signingKeys.publicKey};
+    var privateSigningKey = {bytes: signingKeys.secretKey};
 
-    let userKeys, deviceKeys;
-    ioToFunc(PreCrypt.generateKeyPair, console.log, (user) => userKeys = user);
-    ioToFunc(PreCrypt.generateKeyPair, console.log, (device) => deviceKeys = device);
+    var userKeys, deviceKeys;
+    ioToFunc(PreCrypt.generateKeyPair, console.log, function(user) {
+        userKeys = user;
+    });
+    ioToFunc(PreCrypt.generateKeyPair, console.log, function(device) {
+        deviceKeys = device;
+    });
 
-    ioToFunc(PreCrypt.generatePlaintext, console.log, (plaintext) => {
-        ioToFunc(PreCrypt.generateTransformKey(userKeys.privateKey, deviceKeys.publicKey, publicSigningKey, privateSigningKey), console.log, (transformKey) => {
-            ioToFunc(PreCrypt.encrypt(plaintext, userKeys.publicKey, publicSigningKey, privateSigningKey), console.log, (encryptedMessage) => {
-                ioToFunc(PreCrypt.transform(encryptedMessage, transformKey, publicSigningKey, privateSigningKey), console.log, (transformedMessage) => {
+    ioToFunc(PreCrypt.generatePlaintext, console.log, function(plaintext) {
+        ioToFunc(PreCrypt.generateTransformKey(userKeys.privateKey, deviceKeys.publicKey, publicSigningKey, privateSigningKey), console.log, function(
+            transformKey
+        ) {
+            ioToFunc(PreCrypt.encrypt(plaintext, userKeys.publicKey, publicSigningKey, privateSigningKey), console.log, function(encryptedMessage) {
+                ioToFunc(PreCrypt.transform(encryptedMessage, transformKey, publicSigningKey, privateSigningKey), console.log, function(transformedMessage) {
                     done(deviceKeys.privateKey, transformedMessage);
                 });
             });
@@ -23,23 +29,39 @@ function generateEncryptedLevel1(PreCrypt, signingKeys, done){
 /**
  * Generate a self contained level 2 encrypted value. Results in the encrypted value and the private key that can be used to decrypt that value.
  */
-function generateEncryptedLevel2(PreCrypt, signingKeys, done){
-    const publicSigningKey = {bytes: signingKeys.publicKey};
-    const privateSigningKey = {bytes: signingKeys.secretKey};
+function generateEncryptedLevel2(PreCrypt, signingKeys, done) {
+    var publicSigningKey = {bytes: signingKeys.publicKey};
+    var privateSigningKey = {bytes: signingKeys.secretKey};
 
-    let groupKeys, userKeys, deviceKeys;
-    ioToFunc(PreCrypt.generateKeyPair, console.log, (group) => groupKeys = group);
-    ioToFunc(PreCrypt.generateKeyPair, console.log, (user) => userKeys = user);
-    ioToFunc(PreCrypt.generateKeyPair, console.log, (device) => deviceKeys = device);
+    var groupKeys, userKeys, deviceKeys;
+    ioToFunc(PreCrypt.generateKeyPair, console.log, function(group) {
+        groupKeys = group;
+    });
+    ioToFunc(PreCrypt.generateKeyPair, console.log, function(user) {
+        userKeys = user;
+    });
+    ioToFunc(PreCrypt.generateKeyPair, console.log, function(device) {
+        deviceKeys = device;
+    });
 
-    ioToFunc(PreCrypt.generatePlaintext, console.log, (plaintext) => {
-        ioToFunc(PreCrypt.generateTransformKey(groupKeys.privateKey, userKeys.publicKey, publicSigningKey, privateSigningKey), console.log, (groupToUserTransform) => {
-            ioToFunc(PreCrypt.generateTransformKey(userKeys.privateKey, deviceKeys.publicKey, publicSigningKey, privateSigningKey), console.log, (userToDeviceTransform) => {
-                ioToFunc(PreCrypt.encrypt(plaintext, groupKeys.publicKey, publicSigningKey, privateSigningKey), console.log, (encryptedMessage) => {
-                    ioToFunc(PreCrypt.transform(encryptedMessage, groupToUserTransform, publicSigningKey, privateSigningKey), console.log, (transformedToUserMessage) => {
-                        ioToFunc(PreCrypt.transform(transformedToUserMessage, userToDeviceTransform, publicSigningKey, privateSigningKey), console.log, (transformedToDeviceMessage) => {
-                            done(deviceKeys.privateKey, transformedToDeviceMessage);
-                        });
+    ioToFunc(PreCrypt.generatePlaintext, console.log, function(plaintext) {
+        ioToFunc(PreCrypt.generateTransformKey(groupKeys.privateKey, userKeys.publicKey, publicSigningKey, privateSigningKey), console.log, function(
+            groupToUserTransform
+        ) {
+            ioToFunc(PreCrypt.generateTransformKey(userKeys.privateKey, deviceKeys.publicKey, publicSigningKey, privateSigningKey), console.log, function(
+                userToDeviceTransform
+            ) {
+                ioToFunc(PreCrypt.encrypt(plaintext, groupKeys.publicKey, publicSigningKey, privateSigningKey), console.log, function(encryptedMessage) {
+                    ioToFunc(PreCrypt.transform(encryptedMessage, groupToUserTransform, publicSigningKey, privateSigningKey), console.log, function(
+                        transformedToUserMessage
+                    ) {
+                        ioToFunc(
+                            PreCrypt.transform(transformedToUserMessage, userToDeviceTransform, publicSigningKey, privateSigningKey),
+                            console.log,
+                            function(transformedToDeviceMessage) {
+                                done(deviceKeys.privateKey, transformedToDeviceMessage);
+                            }
+                        );
                     });
                 });
             });
