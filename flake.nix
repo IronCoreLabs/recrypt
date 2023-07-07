3013ae04afe26602dd8d13cc20505f790b39c367
Colt Frederickson <coltfred@gmail.com>
2023-07-07 16:44:25 -0600

    Add flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+
+        mkShell = jdk: pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "recrypt";
+          typelevelShell = {
+            jdk.package = jdk;
+          };
+        };
+      in
+      rec {
+        devShell = devShells."temurin@11";
+
+        devShells = {
+          "temurin@11" = mkShell pkgs.temurin-bin-11;
+          "temurin@17" = mkShell pkgs.temurin-bin-17;
+        };
+      }
+    );
+}
+
